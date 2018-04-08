a16da08dd6c8104090a75f7d66f02c3d63adac98
Ozana Buljan <ozana.buljan@gmail.com>
2018-04-08 14:47:57 +0200

    initial basic working version

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*jQuery*/
 $(document).ready(function () {
 
-    /* *** *** *** VARIABLES *** *** *** */
+    /* *** *** *** *** *** VARIABLES *** *** *** *** *** */
 
     // Element with ID game -> save as variable
     const game = $('#game');
@@ -78,16 +78,21 @@ $(document).ready(function () {
     game.append(deck);
 
     let selectedCards = []; //array to hold selected cards per turn
-
-
-    let firstPick = '';
-    let secondPick = '';
-    let count = 0;
+    let firstCard = ''; // first selected card per turn
+    let secondCard = ''; //second selected card per turn
+    let count = 0; //counts moves per turn
     let previousTarget = null;
 
+    const time = $(".time");
+    let minutes = $(".minutes");
+    let seconds = $(".seconds");
+    let timer;
 
-    /* *** *** *** FUNCTIONS *** *** *** */
 
+
+    /* *** *** *** *** *** FUNCTIONS *** *** *** *** *** */
+
+    /* *** *** SHUFFLE *** *** */
     //Shuffle function from http://stackoverflow.com/a/2450976
     function shuffle(array) {
         let currentIndex = array.length,
@@ -104,40 +109,84 @@ $(document).ready(function () {
         return array;
     }
 
-    // display cards function - by iterating over cards array; creates .card li element and attaches it to .deck ul element
-    const displayCards = function () {
+    /* *** *** DISPLAY CARDS *** *** */
+    //  Display the cards on the page  - by iterating over cards array; creates .card li element and attaches it to .deck ul element
 
-        //before game starts shuffle the cards
-        shuffle(cards);
-
+    function displayCards() {
+        // *   - loop through each card and create its HTML
         for (const item of cards) {
             const {
                 name,
                 img
             } = item;
 
+            //creates div with card class and adds the attribute data-name which will be basis to compare if two cards match
             const card = $('<li></li>').addClass('card');
             card.attr('data-name', item.name);
 
+
+            //creates front-> unflipped and back
+            // -> flipped, with image-  view of the card and
             const front = $('<div></div>').addClass('front');
             const back = $('<div></div>').addClass('back');
             let imageUrl = `url(${img})`;
             back.css('background-image', imageUrl);
 
+            //*   - add each card's HTML to the page
             deck.append(card);
             card.append(front);
             card.append(back);
         }
+
+        //before game starts shuffle the cards
+        shuffle(cards);
     };
-    const match = function () {
+
+
+
+    /* *** *** GAME PLAY *** *** */
+    //Function - Main game play
+
+
+
+    function play() {
+        //resets all the previous moves and empties deck
+        deck.empty();
+        //before game starts shuffle the cards
+        shuffle(cards);
+        displayCards();
+
+        //function to handle events on cards
+        eventHandler();
+        //reset timer after game ends
+        resettimer = 0;
+        seconds.innerText = 0;
+        minutes.innerText = 0;
+        stopTimer();
+
+    };
+
+    /* *** *** MATCH & UNMATCH FUNCTIONS *** *** */
+    function checkIfMatch(firstCard, secondCard) {
+        if (firstCard && secondCard) {
+            if (firstCard === secondCard) {
+                setTimeout(match, 1200);
+            }
+            setTimeout(unmatch, 1200);
+        }
+    }
+
+    function match() {
         const selected = document.querySelectorAll('.selected');
         selected.forEach(card => {
             card.classList.add('match');
+            card.classList.add('open');
         });
     };
-    const resetPicks = function () {
-        firstPick = '';
-        secondPick = '';
+
+    function unmatch() {
+        firstCard = '';
+        secondCard = '';
         count = 0;
         previousTarget = 0;
 
@@ -146,56 +195,89 @@ $(document).ready(function () {
             card.classList.remove('selected');
         });
     };
-
-    const play = function () {
-        //resets all the previous moves and empties deck
-        deck.empty();
+    /* *** *** MOVES COUNTER *** *** */
 
 
-        //before game starts shuffle the cards
-        shuffle(cards);
+    /* *** *** STAR RATING *** *** */
 
-        displayCards();
 
-    };
+
+    function startTimer() {
+        timer = setInterval(function () {
+            seconds.innerText++;
+            if (seconds.innerText == 60) {
+                minutes.innerText++;
+                seconds.innerText = 0;
+            }
+        }, (1000));
+        return timer;
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+
+
+    /* *** *** TIMER *** *** */
+
+
+
+    /* *** *** MODAL *** *** */
+
 
     /* *** *** *** EVENT LISTENERS*** *** *** */
+    //Event listener ->  if a card is clicked: display the card's symbol
+
+    //Event listener ->  if a card is clicked: add the card to a *list* of "open" cards
+
+    //Event listener ->   if the list already has another card, check to see if the two cards match
+
+    // if the cards do match, lock the cards in the open position
+
+    //Event listener ->   if the list already has another card, check to see if the two cards match
+
+    //Event listener ->   if the list already has another card, check to see if the two cards match
+
+
+
+
+
     //Event listener -> on click flip the card
-    deck.on('click', event => {
+    function eventHandler() {
+        deck.on('click', event => {
 
-        const clicked = event.target;
+            const clicked = event.target;
 
-        if (
-            clicked.nodeName === 'UL' ||
-            clicked === previousTarget ||
-            clicked.parentNode.classList.contains('selected')
-        ) {
-            return;
-        }
-
-        if (count < 2) {
-            count++;
-            if (count === 1) {
-                firstPick = clicked.parentNode.dataset.name;
-                console.log(firstPick);
-                clicked.parentNode.classList.add('selected');
-            } else {
-                secondPick = clicked.parentNode.dataset.name;
-                console.log(secondPick);
-                clicked.parentNode.classList.add('selected');
+            if (
+                clicked.nodeName === 'UL' ||
+                clicked === previousTarget ||
+                clicked.parentNode.classList.contains('selected')
+            ) {
+                return;
             }
-
-            if (firstPick && secondPick) {
-                if (firstPick === secondPick) {
-                    setTimeout(match, 1200);
+            //Start timer on first click on a card
+            if (resettimer === 0) {
+                startTimer();
+                resettimer = 1;
+            }
+            if (count < 2) {
+                count++;
+                if (count === 1) {
+                    firstCard = clicked.parentNode.dataset.name;
+                    console.log(firstCard);
+                    clicked.parentNode.classList.add('selected');
+                } else {
+                    secondCard = clicked.parentNode.dataset.name;
+                    console.log(secondCard);
+                    clicked.parentNode.classList.add('selected');
                 }
-                setTimeout(resetPicks, 1200);
+
+                checkIfMatch(firstCard, secondCard);
+                previousTarget = clicked;
             }
-            previousTarget = clicked;
-        }
 
-    });
-
+        });
+    }
     /* *** *** *** CALLING FUNCTIONS *** *** *** */
     play();
 
@@ -208,31 +290,6 @@ $(document).ready(function () {
 
 
 /* Create a list that holds all of your cards*/
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-/*
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
 
 
 /*
