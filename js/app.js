e49330280d5660e4002b2be202c6e687683a7ad6
Ozana Buljan <ozana.buljan@gmail.com>
2018-04-15 21:50:44 +0200

    displays deck with JS

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
-/*jQuery*/
-$(document).ready(function () {
-
-    /* *** *** *** *** *** VARIABLES *** *** *** *** *** */
-
-    // Element with ID game -> save as variable
-    const game = $('#game');
-
-
-    // Array which holds cards with unique images -> half of deck
-    const cardsUnique = [
+/* *** *** *** *** *** VARIABLES *** *** *** *** *** */
+const cardsUnique = [
     {
         id: 1,
         name: 'bear',
@@ -87,243 +78,91 @@ $(document).ready(function () {
         },
     ];
 
-    //creating pairs by duplicating card array
-    const cards = cardsUnique
-        .concat(cardsUnique);
+//creating pairs by duplicating card array
+const cards = cardsUnique
+    .concat(cardsUnique);
 
-    const deck = $('<ul></ul>').addClass('deck');
-    game.append(deck);
-
-    let open = []; //array to hold selected cards per turn
-
-    let firstCard = ''; // first selected card per turn
-    let secondCard = ''; //second selected card per turn
-    let count = 0; //counts moves per turn
-    let previousTarget = null;
+const deck = document.querySelector('.deck'); //element which contains all cards
+const card = document.querySelectorAll(".card"); // card element
 
 
-    let moves = $('.moves');
+let firstCard = ""; //first card clicked in a turn
+let secondCard = ""; //second card clicked in a turn
+let openCards = []; //array which holds open cards
+let clicks = 0; //counts clicks on cards -> 2 clicks is one turn
+let pairs = []; //counts matched pairs - > game finished when all 15 pairs are matched
 
-    /* *** *** *** *** *** FUNCTIONS *** *** *** *** *** */
 
-    /* *** *** SHUFFLE *** *** */
-    //Shuffle function from http://stackoverflow.com/a/2450976
-    function shuffle(array) {
-        let currentIndex = array.length,
-            temporaryValue, randomIndex;
 
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
 
-        return array;
+/* *** *** *** *** *** FUNCTIONS *** *** *** *** *** */
+
+/* *** *** SHUFFLE *** *** */
+//Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
-    /* *** *** DISPLAY CARDS *** *** */
-    //  Display the cards on the page  - by iterating over cards array; creates .card li element and attaches it to .deck ul element
+    return array;
+}
+shuffle(cards);
 
-    function displayCards() {
-        // *   - loop through each card and create its HTML
-        for (const item of cards) {
-            const {
-                id,
-                name,
-                img
-            } = item;
+/* *** *** CREATE DECK *** *** */
 
-            //creates div with card class and adds the attribute data-name which will be basis to compare if two cards match
-            const card = $('<li></li>').addClass('card');
-            card.attr('data-name', item.name);
+// @description -> by iterating over cards array; it attaches the images and sets card's id attribute
 
-
-            //creates front-> unflipped and back
-            // -> flipped, with image-  view of the card and
-            const front = $('<div></div>').addClass('front');
-            const back = $('<div></div>').addClass('back');
-            let imageUrl = `url(${img})`;
-            back.css('background-image', imageUrl);
-
-            //*   - add each card's HTML to the page
-            deck.append(card);
-            card.append(front);
-            card.append(back);
-        }
-
-        //before game starts shuffle the cards
-        shuffle(cards);
-    };
+function createDeck() {
+    for (let i = 0; i < card.length; i++) {
+        card[i].children.item(0).setAttribute("src", cards[i].img);
+        card[i].setAttribute("id", cards[i].id);
+    }
+}
+/* *** *** PLAY *** *** */
+deck.addEventListener('click', function play(clicked) {
 
 
+    createDeck();
 
-    /* *** *** GAME PLAY *** *** */
-    //Function - Main game play
+    if (clicked.target.nodeName == 'LI') {
+        if (clicks < 2) {
+            clicks++;
+            if (clicks === 1) {
+                firstCard = clicked.target;
+                firstCard.classList.add("open");
+                firstCard.classList.add("show");
+                let cardId = clicked.target.getAttribute("id");
+                openCards.push(cardId);
 
 
+            } else if (clicks === 2) {
+                secondCard = clicked.target;
+                secondCard.classList.add("open");
+                secondCard.classList.add("show");
+                let cardId = clicked.target.getAttribute("id");
+                openCards.push(cardId);
 
-    function play() {
-        //resets all the previous moves and empties deck
-        deck.empty();
-        movesCount = 0;
-        moves.html(movesCount);
-
-
-
-        //create new deck
-        displayCards();
-        //before game starts shuffle the cards
-        shuffle(cards);
-        //function to handle events on cards
-        eventHandler();
-
-        //reset timer after game ends
-
-    };
-
-    /* *** *** MATCH & UNMATCH FUNCTIONS *** *** */
-    function checkIfMatch(firstCard, secondCard) {
-        if (firstCard && secondCard) {
-            if (firstCard === secondCard) {
-                setTimeout(match, 1200);
             }
-            setTimeout(unmatch, 1200);
-        }
-    }
-
-    function match() {
-        const selected = document.querySelectorAll('.selected');
-        selected.forEach(card => {
-            card.classList.add('match');
-            card.classList.add('open');
-        });
-    };
-
-    function unmatch() {
-        firstCard = '';
-        secondCard = '';
-        count = 0;
-        previousTarget = 0;
-
-        let selected = document.querySelectorAll('.selected');
-        selected.forEach(card => {
-            card.classList.remove('selected');
-        });
-    };
-
-    /* *** *** MOVES COUNTER *** *** */
-
-    const moves = document.querySelector(".moves");
-    let countingMoves = 0;
-    const star = document.querySelectorAll('.fa-star');
-    const starOne = star[0];
-    const starTwo = star[1];
-
-    function starRemoval() {
-        countingMoves++;
-        moves.innerText = countingMoves;
-
-        if (countingMoves == 1) {
-            startTimer();
         }
 
-        if (countingMoves == 10) {
-            starOne.className += " hidden";
-
-        } else if (countingMoves == 16) {
-            starTwo.className += " hidden";
+        if (openCards.length == 2) {
+            checkIfMatch();
+            starRating();
+            setTimeout(resetCards, 1200);
+        }
+        if (pairs.length === 15) {
+            openModal();
         }
     }
-    /* *** *** STAR RATING *** *** */A
+})
 
-
-    /* *** *** TIMER *** *** */
-
-
-
-    /* *** *** MODAL *** *** */
-
-    /* *** *** RESET *** *** */
-    const restart = $(".restart");
-    restart.addEventListener("click", function buttonReset() {
-
-        if (firstCard != "") {
-            firstCard.classList.remove("open", "show")
-        };
-        removeMatch();
-
-        resetCards();
-
-        openedCards = [];
-        countingMoves = 0;
-        moves.innerText = 0;
-        secondsLabel.innerHTML = 0;
-        matchedCardsInArray = [];
-        totalSeconds = 0;
-        starOne.classList.remove("hidden");
-        starTwo.classList.remove("hidden");
-        clearInterval(interval);
-        shuffle(fullDeck);
-
-
-    })
-
-    /* *** *** *** EVENT LISTENERS*** *** *** */
-    //Event listener ->  if a card is clicked: display the card's symbol
-
-    //Event listener ->  if a card is clicked: add the card to a *list* of "open" cards
-
-    //Event listener ->   if the list already has another card, check to see if the two cards match
-
-    // if the cards do match, lock the cards in the open position
-
-    //Event listener ->   if the list already has another card, check to see if the two cards match
-
-    //Event listener ->   if the list already has another card, check to see if the two cards match
-
-
-
-
-
-    //Event listener -> on click flip the card
-    function eventHandlers() {
-        deck.on('click', event => {
-
-            const clicked = event.target;
-
-            if (
-                clicked.nodeName === 'UL' ||
-                clicked === previousTarget ||
-                clicked.parentNode.classList.contains('selected')
-            ) {
-                return;
-            }
-
-            if (count < 2) {
-                count++;
-                if (count === 1) {
-                    firstCard = clicked.parentNode.dataset.name;
-                    console.log(firstCard);
-                    clicked.parentNode.classList.add('selected');
-                } else {
-                    secondCard = clicked.parentNode.dataset.name;
-                    console.log(secondCard);
-                    clicked.parentNode.classList.add('selected');
-                }
-
-                checkIfMatch(firstCard, secondCard);
-                previousTarget = clicked;
-            }
-
-        });
-    }
-    /* *** *** *** CALLING FUNCTIONS *** *** *** */
-    play();
-
-
-}); // end jQuery
 
 
 
@@ -336,9 +175,7 @@ $(document).ready(function () {
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- deck.on('click', function(){
 
- });
  function showCard(){}
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
 function addCardtoOpen(){}
